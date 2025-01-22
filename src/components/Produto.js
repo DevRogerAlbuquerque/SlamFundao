77f65ebeddcdba9c055513115e9e326f1a92f038
RogerAlbuquerqueBR <contatorogeroficial@gmail.com>
2025-01-22 20:48:12 -0300

    Iniciando parte de detalhamento de produtos, que terá informações sobre preços e tamanhos, e a opção de comprar.

--- src/components/Produto.js
+++ src/components/Produto.js
@@ -3,18 +3,23 @@ import './styles/produto.css';
 import { FaEye } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 
-export const Produto = ({src, alt, descricao}) => {
-  const navigate = useNavigate();
+export const Produto = ({src, alt, descricao, valor, onClick}) => {
+  const options = { style: 'currency', currency: 'BRL', minimumFractionDigits: 2, maximumFractionDigits: 3 }
+  const formatNumber = new Intl.NumberFormat('pt-BR', options)
   return (
     <Container className="produto" style={{ width: '100%' }} fluid>
       <Row>
         <Col className="p-0 mx-3">
           <img src={src} height={"470px"} alt={alt} />
-          <Button className="comprar-button" onClick={() => navigate('../compra')}><FaEye /> Visualizar</Button>
+          <Button className="comprar-button" onClick={onClick}><FaEye /> Visualizar</Button>
         </Col>
       </Row>
       <Row>
-        <Col className="descricaoProduto rounded-bottom mx-3" style={{backgroundColor: 'rgb(78, 39, 89)', color: 'white'}}>{descricao}</Col>
+        <Col className="descricao mx-3" style={{backgroundColor: '#FFC670', border: '5px solid rgb(78, 39, 89)'}}>{descricao}</Col>
+      </Row>
+      <Row>
+        <Col className="descricao rounded-bottom mx-3" style={{backgroundColor: 'rgb(78, 39, 89)', 
+          color: 'white', borderBottom: '1px solid black'}}>Valor: {formatNumber.format(valor)}</Col>
       </Row>
     </Container>
   );
--- src/routes/produtos/index.js
+++ src/routes/produtos/index.js
@@ -1,30 +1,74 @@
-import { Col, Row, Container } from "react-bootstrap";
-import { useNavigate, useOutletContext } from "react-router-dom";
+import { Col, Row, Container, Carousel, Card } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 import './index.css';
 import Produto from "../../components/Produto";
 import camisaBranca from '../../imagens/camisaBranca2.jpg';
 import camisaAmarela from '../../imagens/camisaAmarela.jpg';
 import camisaRoxa from '../../imagens/brancaRoxa.jpg';
-import camisaBranca2 from '../../imagens/camisaBranca.jpg';
+import { useState } from "react";
 
-export default function Produtos()
-{
-    const navigate = useNavigate();
+export default function Produtos() {
+    const [catalogoVisible, showCatalogo] = useState(true);
+    const [produtoDetalhado, setProdutoDetalhado] = useState({});
 
-    return (<>
-    <Container fluid className="d-flex flex-column align-items-center justify-content-center" style={{height: '100vh'}}>
-        <h1 className="roxo">DROP DO FUNDÃO</h1>
-        <Row>
-            <Col sm={12} md={4} className="mb-3 p-0">
-                <Produto src={camisaBranca} descricao={"CAMISETA OFF WHITE - Valor: R$50"} />
-            </Col>
-            <Col sm={12} md={4} className="mb-3 p-0">
-                <Produto descricao={"CAMISETA AMARELA - Valor: R$50"} src={camisaAmarela} />
-            </Col>
-            <Col sm={12} md={4} className="mb-3 p-0">
-                <Produto descricao={"CAMISETA ROXA - Valor: R$50"} src={camisaRoxa} />
-            </Col>
-        </Row>
-    </Container>
-    </>)
+    const produtos = [
+        { nome: 'Camiseta Off-White', valor: 50, imagem: camisaBranca, imagens: 'images/camisetas/branca/' },
+        { nome: 'Camiseta Amarela', valor: 50, imagem: camisaAmarela, imagens: 'images/camisetas/amarela/' },
+        { nome: 'Camiseta Roxa', valor: 50, imagem: camisaRoxa, imagens: 'images/camisetas/roxa/' },
+    ];
+
+    return (
+        <Container fluid style={{ height: '90vh', marginTop: '10vh' }}>
+            {catalogoVisible && 
+                <><Row>
+                    <Col><h1 className="roxo mt-3">DROP DO FUNDÃO</h1></Col>
+                </Row>
+                <Row>
+                    {produtos.map((produto, index) => (
+                        <Col
+                            key={index}
+                            xs={12}
+                            sm={12}
+                            md={4} >
+                            <Produto
+                                valor={produto.valor}
+                                src={produto.imagem}
+                                descricao={produto.nome}
+                                onClick={() => {
+                                    showCatalogo(false);
+                                    setProdutoDetalhado(produto);
+                                }}
+                            />
+                        </Col>
+                    ))}
+                </Row>
+                </>
+            }
+            {!catalogoVisible && 
+                <>
+                    <Row>
+                        <Col><h1 className="roxo mt-3">{produtoDetalhado.nome}</h1></Col>
+                    </Row>
+                    <Row>
+                        <Col md={4}  className="d-flex align-items-center justify-content-center">
+                            <Carousel style={{border: '15px solid #4E2759', borderRadius: '5%'}}>
+                                {[1, 2, 3, 4].map(item => 
+                                    <Carousel.Item>
+                                        <img style={{height: '70vh'}}
+
+                                        src={`${produtoDetalhado.imagens}${item}.jpg`}/>
+                                    </Carousel.Item>
+                                )}
+                            </Carousel>
+                        </Col>
+                        <Col md={8} className="backgroundRoxo" style={{borderRadius: '5%'}}>
+                                <Container fluid>
+                                    
+                                </Container>
+                        </Col>
+                    </Row>
+                </>
+            }
+        </Container>
+    );
 }
